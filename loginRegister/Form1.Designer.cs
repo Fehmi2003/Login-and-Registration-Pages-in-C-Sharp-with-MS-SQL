5190bc0bf2535352dfbed69bc45915665a2043a7
FehmiTD <fehmitahsindemirkan@gmail.com>
2023-11-30 02:39:00 +0300

    Add project files.

---
+++ loginRegister/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace loginRegister
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtisim = new System.Windows.Forms.TextBox();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.txtsifretekrar = new System.Windows.Forms.TextBox();
+            this.checkboxsifregoster = new System.Windows.Forms.CheckBox();
+            this.btnkayitol = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TUR UYGULAMASI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(40, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İsim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label3.Location = new System.Drawing.Point(40, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Şifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label4.Location = new System.Drawing.Point(40, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şifre Tekrar";
+            // 
+            // txtisim
+            // 
+            this.txtisim.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtisim.Location = new System.Drawing.Point(44, 75);
+            this.txtisim.Name = "txtisim";
+            this.txtisim.Size = new System.Drawing.Size(164, 30);
+            this.txtisim.TabIndex = 4;
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtsifre.Location = new System.Drawing.Point(44, 146);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.Size = new System.Drawing.Size(164, 30);
+            this.txtsifre.TabIndex = 5;
+            // 
+            // txtsifretekrar
+            // 
+            this.txtsifretekrar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtsifretekrar.Location = new System.Drawing.Point(44, 218);
+            this.txtsifretekrar.Name = "txtsifretekrar";
+            this.txtsifretekrar.Size = new System.Drawing.Size(164, 30);
+            this.txtsifretekrar.TabIndex = 6;
+            // 
+            // checkboxsifregoster
+            // 
+            this.checkboxsifregoster.AutoSize = true;
+            this.checkboxsifregoster.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkboxsifregoster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkboxsifregoster.ForeColor = System.Drawing.Color.BurlyWood;
+            this.checkboxsifregoster.Location = new System.Drawing.Point(68, 276);
+            this.checkboxsifregoster.Name = "checkboxsifregoster";
+            this.checkboxsifregoster.Size = new System.Drawing.Size(139, 27);
+            this.checkboxsifregoster.TabIndex = 7;
+            this.checkboxsifregoster.Text = "Şifreyi Göster";
+            this.checkboxsifregoster.UseVisualStyleBackColor = true;
+            // 
+            // btnkayitol
+            // 
+            this.btnkayitol.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnkayitol.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnkayitol.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnkayitol.FlatAppearance.BorderSize = 0;
+            this.btnkayitol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnkayitol.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnkayitol.Location = new System.Drawing.Point(31, 320);
+            this.btnkayitol.Name = "btnkayitol";
+            this.btnkayitol.Size = new System.Drawing.Size(216, 35);
+            this.btnkayitol.TabIndex = 8;
+            this.btnkayitol.Text = "KAYIT OL";
+            this.btnkayitol.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button1.Location = new System.Drawing.Point(31, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "TEMİZLE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(40, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Zaten Hesabın Var mı ?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label6.Location = new System.Drawing.Point(26, 467);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(222, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Giriş Yapma Ekranına Git";
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 544);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnkayitol);
+            this.Controls.Add(this.checkboxsifregoster);
+            this.Controls.Add(this.txtsifretekrar);
+            this.Controls.Add(this.txtsifre);
+            this.Controls.Add(this.txtisim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FloralWhite;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtisim;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.TextBox txtsifretekrar;
+        private System.Windows.Forms.CheckBox checkboxsifregoster;
+        private System.Windows.Forms.Button btnkayitol;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ loginRegister/frmLogin.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace loginRegister
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnkayitol = new System.Windows.Forms.Button();
+            this.checkboxsifregoster = new System.Windows.Forms.CheckBox();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.txtisim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label6.Location = new System.Drawing.Point(108, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 25);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Hesap Oluştur";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(92, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 23);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Hesabın Yok mu ?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button1.Location = new System.Drawing.Point(56, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "TEMİZLE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnkayitol
+            // 
+            this.btnkayitol.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnkayitol.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnkayitol.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnkayitol.FlatAppearance.BorderSize = 0;
+            this.btnkayitol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnkayitol.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnkayitol.Location = new System.Drawing.Point(56, 271);
+            this.btnkayitol.Name = "btnkayitol";
+            this.btnkayitol.Size = new System.Drawing.Size(216, 35);
+            this.btnkayitol.TabIndex = 20;
+            this.btnkayitol.Text = "GİRİŞ YAP";
+            this.btnkayitol.UseVisualStyleBackColor = false;
+            // 
+            // checkboxsifregoster
+            // 
+            this.checkboxsifregoster.AutoSize = true;
+            this.checkboxsifregoster.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkboxsifregoster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkboxsifregoster.ForeColor = System.Drawing.Color.BurlyWood;
+            this.checkboxsifregoster.Location = new System.Drawing.Point(90, 219);
+            this.checkboxsifregoster.Name = "checkboxsifregoster";
+            this.checkboxsifregoster.Size = new System.Drawing.Size(139, 27);
+            this.checkboxsifregoster.TabIndex = 19;
+            this.checkboxsifregoster.Text = "Şifreyi Göster";
+            this.checkboxsifregoster.UseVisualStyleBackColor = true;
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtsifre.Location = new System.Drawing.Point(79, 172);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.Size = new System.Drawing.Size(164, 30);
+            this.txtsifre.TabIndex = 17;
+            // 
+            // txtisim
+            // 
+            this.txtisim.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtisim.Location = new System.Drawing.Point(79, 101);
+            this.txtisim.Name = "txtisim";
+            this.txtisim.Size = new System.Drawing.Size(164, 30);
+            this.txtisim.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label3.Location = new System.Drawing.Point(75, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Şifre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(75, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "İsim";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(75, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "TUR UYGULAMASI";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(361, 703);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnkayitol);
+            this.Controls.Add(this.checkboxsifregoster);
+            this.Controls.Add(this.txtsifre);
+            this.Controls.Add(this.txtisim);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnkayitol;
+        private System.Windows.Forms.CheckBox checkboxsifregoster;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.TextBox txtisim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
